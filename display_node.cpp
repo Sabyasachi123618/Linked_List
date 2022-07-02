ae550968b89c9c3a180bfb008774cfa03365365f
Sabyasachi Banerjee <57725708+Sabyasachi123618@users.noreply.github.com>
2022-07-03 00:46:53 +0530

    Create display_node.cpp

---
+++ display_node.cpp
@@ -0,0 +1,10 @@
+void display(Node *head)
+    {
+      //your code goes here
+      Node *temp=head;
+      while(temp!=NULL)
+      {
+          cout<<temp->data<<" ";
+          temp=temp->next;
+      }
+    }
